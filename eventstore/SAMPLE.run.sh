5e5b910642a73def9369412bd9d3c56ec1b987e2
luminescent <christina.androne@gmail.com>
2015-12-28 12:32:14 +0000

    removed unused port

--- eventstore/SAMPLE.run.sh
+++ eventstore/SAMPLE.run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -i -t -p 2113:2113 -p 1113:1113 -p 2213:2213 \
+docker run -i -t -p 2113:2113 -p 1113:1113 \
 	-v /home/christina/projects/temp/data/db:/data/db  -v /home/christina/projects/temp/data/logs:/data/logs \
 	eventstore:latest \
 	--ext-http-prefixes=http://*:2113/ --ext-ip=0.0.0.0 --db /data/db --log /data/logs
