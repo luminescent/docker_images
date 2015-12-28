4cf2db5046b42793b1c938e897c1316d4246f842
luminescent <christina.androne@gmail.com>
2015-12-28 12:27:02 +0000

    some reformatting

--- eventstore/SAMPLE.run.sh
+++ eventstore/SAMPLE.run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -i -t -p 2113:2113 -p 1113:1113 -p 2213:2213 -v /home/christina/projects/temp/data/db:/data/db  -v /home/christina/projects/temp/data/logs:/data/logs eventstore:latest --ext-http-prefixes=http://*:2113/ --ext-ip=0.0.0.0 --db /data/db --log /data/logs 
+docker run -i -t -p 2113:2113 -p 1113:1113 -p 2213:2213 \
+	-v /home/christina/projects/temp/data/db:/data/db  -v /home/christina/projects/temp/data/logs:/data/logs \
+	eventstore:latest \
+	--ext-http-prefixes=http://*:2113/ --ext-ip=0.0.0.0 --db /data/db --log /data/logs
