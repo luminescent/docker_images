9ca7e40a6ca443ab4ce095b57f0d589f1ae9d9cb
luminescent <christina.androne@gmail.com>
2016-01-04 20:56:01 +0000

    a bit of cleanup; fixed the bd and logs folders

--- eventstore/SAMPLE.run.sh
+++ eventstore/SAMPLE.run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run -i -t -p 2113:2113 -p 1113:1113 \
-	-v /home/christina/projects/temp/data/db:/data/db  -v /home/christina/projects/temp/data/logs:/data/logs \
+docker run -i -t \
+	-p 2113:2113 -p 1113:1113 \
+	-v /home/christina/projects/temp/data/db:/db  -v /home/christina/projects/temp/data/logs:/logs \
 	eventstore:latest \
-	--ext-http-prefixes=http://*:2113/ --ext-ip=0.0.0.0 --db /data/db --log /data/logs
+	--ext-http-prefixes=http://*:2113/ --ext-ip=0.0.0.0 
